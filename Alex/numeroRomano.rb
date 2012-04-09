3427cb765547b8f1b0dcacd51c45aa6d6c017ca4
Luis Alexander Aldazabal Gil <berczeck_400@hotmail.com>
2012-04-09 13:38:06 -0500

    Creacion de la primera prueba 5 a V

--- Alex/numeroRomano.rb
+++ Alex/numeroRomano.rb
@@ -1,5 +1,11 @@
 class NumeroRomano
 	def convierte(numero)
-		"I"
+		if numero == 1
+			result = "I"				
+		end
+		if numero == 5
+			result = "V"
+		end
+		return result
 	end
 end
--- Alex/numeroRomano_spec.rb
+++ Alex/numeroRomano_spec.rb
@@ -7,5 +7,9 @@ describe NumeroRomano do
 			nroRomano = NumeroRomano.new()
 			nroRomano.convierte(1).should == "I"
 		end
+		it "el valor 5 en V" do
+			nroRomano = NumeroRomano.new()
+			nroRomano.convierte(5).should == "V"
+		end
 	end
 end
